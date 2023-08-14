8802dcb1cde035cae142f628b31da94f25d3d0f6
Gabriel Porto <7gabrielporto@gmail.com>
2023-08-14 13:50:15 -0300

    feat: update section with props

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,29 @@ function App() {
   return (
     <PageContainer>
       <Header />
-      <Section backgroundColor='#181717'>Conteúdo da Seção 1</Section>
-      <Section backgroundColor='#272727'>Conteúdo da Seção 2</Section>
-      <Section backgroundColor='#181717'>Conteúdo da Seção 3</Section>
-      <Section backgroundColor='#272727'>Conteúdo da Seção 4</Section>
+      <Section
+        backgroundColor="#181717"
+        imageSrc="/assets/art1.png"
+        title="Artes da Partida"
+        text="Perfeito para aumentar a visibilidade do jogador, promovendo a aumento do acompanhamento do atleta pelos torcedores."
+        marginTop='200px'
+      />
+      <Section
+        backgroundColor="#272727"
+        imageSrc="/assets/art2.png"
+        title="Compilados das partidas"
+        text="Performance completa do jogador durante a partida, ótimo para acompanhar performance e aumentar a conexão com os torcedores."
+        padding='100px'
+        
+      />
+      <Section
+        backgroundColor="#181717"
+        imageSrc="/assets/art3.png"
+        title="Material completo"
+        text="O mais procurado, o “DVD do Jogador”. O currículo profissional do atleta, essencial para análise dos olheiros e demais profissionais da comissão dos clubes."
+        padding='100px'
+      />
+      
     </PageContainer>
   );
 }
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,26 +1,64 @@
+// src/components/Section.tsx
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledSection = styled.div`
-  height: 100vh;
+const StyledSection = styled.div<{ backgroundColor: string, marginTop?: string, marginBottom?: string}>`
   display: flex;
   align-items: center;
-  justify-content: center;
-  font-size: 2rem;
+  justify-content: space-between;
+  padding: 2rem;
+  background-color: ${props => props.backgroundColor};
   color: #fff;
+  margin-top: ${props => props.marginTop || 0};
+  margin-bottom: ${props => props.marginBottom || 0};
+`;
+
+const SectionContent = styled.div<{padding?: string}>`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: ${props => props.padding || 0};
+`;
+
+const SectionImage = styled.img`
+  width: 65%;
+  max-width: 1000px;
+`;
+
+const SectionText = styled.div`
+  width: 60%;
+  padding-left: 2rem;
+`;
+
+const SectionTitle = styled.h2`
+  font-size: 2.5rem;
+  margin-bottom: 1rem;
 `;
 
 interface SectionProps {
   backgroundColor: string;
-  children: React.ReactNode;
+  imageSrc: string;
+  title: string;
+  text: string;
+  marginTop?: string;
+  marginBottom?: string;
+  padding?: string;
 }
 
-const Section: React.FC<SectionProps> = ({ backgroundColor, children }) => {
-  const sectionStyles = {
-    backgroundColor: backgroundColor,
-  };
-
-  return <StyledSection style={sectionStyles}>{children}</StyledSection>;
+const Section: React.FC<SectionProps> = ({ backgroundColor, imageSrc, title, text, marginTop, marginBottom, padding }) => {
+  return (
+    <StyledSection backgroundColor={backgroundColor} marginTop={marginTop} marginBottom={marginBottom}>
+      <SectionContent padding={padding}>
+        <SectionImage src={imageSrc} alt="Imagem" />
+        <SectionText>
+          <SectionTitle>{title}</SectionTitle>
+          <p>{text}</p>
+        </SectionText>
+      </SectionContent>
+    </StyledSection>
+  );
 };
 
 export default Section;
