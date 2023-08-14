98264c62da504660b4b6204299e9690913ca2dba
Gabriel Porto <7gabrielporto@gmail.com>
2023-08-14 11:55:49 -0300

    feat: add sections on index

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import PageContainer from './components/PageContainer';
+import Section from './components/Section';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-    </div>
+    <PageContainer>
+      <Header />
+      <Section backgroundColor='#181717'>Conteúdo da Seção 1</Section>
+      <Section backgroundColor='#272727'>Conteúdo da Seção 2</Section>
+      <Section backgroundColor='#181717'>Conteúdo da Seção 3</Section>
+      <Section backgroundColor='#272727'>Conteúdo da Seção 4</Section>
+    </PageContainer>
   );
 }
 
