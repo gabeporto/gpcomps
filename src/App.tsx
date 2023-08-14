1d107c37f3ef8e100655b89bb132007071cac0ce
Gabriel Porto <7gabrielporto@gmail.com>
2023-08-14 10:56:49 -0300

    feat: add styled components dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+
+const StyledHeader = styled.header`
+  background-color: #1838;
+  color: #fff;
+  text-align: center;
+  padding: 2rem;
+`;
+
+const StyledTitle = styled.h1`
+  font-size: 2rem;
+`;
+
+const StyledList = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+const StyledListItem = styled.li`
+  margin: 0.5rem 0;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StyledHeader>
+        <StyledTitle>GPcomps</StyledTitle>
+        <p>Conheça os serviços que oferecemos:</p>
+        <StyledList>
+          <StyledListItem>Serviço 1</StyledListItem>
+          <StyledListItem>Serviço 2</StyledListItem>
+          <StyledListItem>Serviço 3</StyledListItem>
+        </StyledList>
+      </StyledHeader>
     </div>
   );
 }
